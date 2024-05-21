2e7b2035b5d41b125f5f64e63c5b1d16a7509769
Halismelih ABAK <hhlsabkk@icloud.com>
2024-05-21 20:07:14 +0300

    Order details edited

---
+++ src/main/java/com/example/demo/dto/cart/ProductItem.java
@@ -0,0 +1,15 @@
+package com.example.demo.dto.cart;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductItem {
+    private int productId;
+    private String productName;
+    private int productPrice;
+    private int quantity;
+}
